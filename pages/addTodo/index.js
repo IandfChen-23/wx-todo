dbb286dbe06c7d8799c5f5ad56fce59289031ee6
chenlei@npnets.com <chenlei@npnets.com>
2020-01-09 11:57:08 +0800

    update

--- pages/addTodo/index.js
+++ pages/addTodo/index.js
@@ -10,7 +10,40 @@ const db = wx.cloud.database();
 Page({
   data:{
     image:null,
-    location:null
+    location:null,
+    imagewidth: 0, // 缩放后的宽
+    imageheight: 0, // 缩放后的高
+  },
+  imageLoad: function (e) {
+    var imageSize = this.imageUtil(e)
+    this.setData({
+      imagewidth: imageSize.imageWidth,
+      imageheight: imageSize.imageHeight
+    })
+  },
+  imageUtil: function (e) {
+    var imageSize = {};
+    var originalWidth = e.detail.width; // 图片原始宽
+    var originalHeight = e.detail.height; // 图片原始高
+    var originalScale = originalHeight / originalWidth; //图片高宽比
+    //获取屏幕宽高
+    wx.getSystemInfo({
+      success: function (res) {
+        var windowWidth = res.windowWidth;
+        var windowHeight = res.windowHeight;
+        var windowscale = windowHeight / windowWidth;//屏幕高宽比
+        if (originalScale < windowscale) { // 图片高宽比小于屏幕高宽比
+          //图片缩放后的宽为屏幕宽
+          imageSize.imageWidth = windowWidth;
+          imageSize.imageHeight = windowHeight /2;
+        } else { // 图片高宽比大于屏幕高宽比
+          //图片缩放后的高为屏幕高
+          imageSize.imageHeight = windowHeight / 2;
+          imageSize.imageWidth = (windowHeight * originalWidth) / originalHeight;
+        }
+      }
+    })
+    return imageSize;
   },
   selectImg:function(){
     const that=this;
